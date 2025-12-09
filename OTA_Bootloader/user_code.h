9a4f64540c98b68f994d21386e01bad1bd3daa68
mohamedcherif-pixel <mohamedcherif@example.com>
2025-12-09 11:06:29 +0100

    Update user_code.h

--- OTA_Bootloader/user_code.h
+++ OTA_Bootloader/user_code.h
@@ -1,22 +1,6 @@
-#define USER_APP_VERSION 34
-#include <TFT_eSPI.h>
-#include "myImage3.h"
-
-TFT_eSPI tft = TFT_eSPI();
-
-#define TFT_BL 4
+#define USER_APP_VERSION 35
 
 void setup() {
-  tft.init();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH);
-
-  // Display the downloaded image
-  tft.setSwapBytes(true); // Swap bytes for correct endianness
-  tft.pushImage(0, 0, 240, 240, myImage3);
 }
 
 void loop() {}
